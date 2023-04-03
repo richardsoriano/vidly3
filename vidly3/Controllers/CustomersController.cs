50ac3501b9d1bb1372a31ecf973eedfd158bbdbf
richardsoriano <soriano.richard2020@gmail.com>
2023-04-03 09:54:46 -0700

    Customer Save Edit to DB

--- vidly3/Controllers/CustomersController.cs
+++ vidly3/Controllers/CustomersController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 using vidly3.Data;
 using vidly3.Models;
+using vidly3.ViewModels;
 
 namespace vidly3.Controllers
 {
@@ -23,11 +25,50 @@ namespace vidly3.Controllers
         }
         public IActionResult New()
         {
-            return View();
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel { 
+                MembershipTypes = membershipTypes 
+            };
+            return View("CustomerForm",viewModel);
+        }
+        public IActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+        };
+            return View("CustomerForm", viewModel);
+        }
+        
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c=> c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.IsSubscribedToNewsletter= customer.IsSubscribedToNewsletter;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.Birthdate= customer.Birthdate;
+                TryUpdateModelAsync(customerInDb);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
         }
         public ActionResult Details(int id)
         {
-            var customers =_context.Customers.SingleOrDefault(c => c.Id == id);
+            var customers =_context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
             return View(customers);
         }
         //private IEnumerable<Customer> GetCustomers()
---
+++ vidly3/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,10 @@
+﻿using vidly3.Models;
+
+namespace vidly3.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
